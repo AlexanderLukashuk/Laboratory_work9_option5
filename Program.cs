8705289b785cd24d5b1438b18dd67daef3a47735
Саня <sanya@Sergejs-MacBook-Air.local>
2021-04-18 00:49:58 +0600

    Создан массив переменные для самого большого числа в массиве и самого маленького и переменная класса Random, создан цикл for для заполнения массива случайными числами, создан второй цикл for для нахождения наибольшего и наименьшего элементов массива

--- Program.cs
+++ Program.cs
@@ -6,7 +6,42 @@ namespace Task9v5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //* Ввести целочисленный массив, состоящий из 9 элементов.
+            //* Поменять местами максимальный и минимальный элементы массива.
+
+            int[] arrayInt = new int[9];
+            Random random = new Random();
+            int maxNumber = 0;
+            int minNumber = 0;
+
+            for (int i = 0; i < arrayInt.Length; i++)
+            {
+                arrayInt[i] = random.Next(100);
+                Console.Write($"{arrayInt[i]} ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < arrayInt.Length; i++)
+            {
+                if (minNumber == 0)
+                {
+                    minNumber = arrayInt[i];
+                }
+                for (int j = i; j < arrayInt.Length; j++)
+                {
+                    if (maxNumber < arrayInt[j])
+                    {
+                        maxNumber = arrayInt[j];
+                    }
+
+                    if (arrayInt[j] < minNumber)
+                    {
+                        minNumber = arrayInt[j];
+                    }
+                }
+            }
+
+            Console.WriteLine($"Max = {maxNumber} Min = {minNumber}");
         }
     }
 }
